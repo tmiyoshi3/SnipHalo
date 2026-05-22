045db544cdb1ab396dc81014516e4f0d205b9182
Teruo Miyoshi <tmiyoshi@redhat.com>
2026-05-22 23:09:09 +0900

    設定UIのテキスト領域でCmd+C,V,Z,Aなど機能するように修正。

--- Sources/QuickSmiley/AppDelegate.swift
+++ Sources/QuickSmiley/AppDelegate.swift
@@ -24,8 +24,34 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         hotkeyManager.register(config: configManager.config.hotkey)
 
+        setupMainMenu()
+
         statusBarController = StatusBarController()
         statusBarController.setup()
+    }
+
+    private func setupMainMenu() {
+        let mainMenu = NSMenu()
+
+        let appMenuItem = NSMenuItem()
+        let appMenu = NSMenu()
+        appMenu.addItem(withTitle: "Quit QuickSmiley", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        appMenuItem.submenu = appMenu
+        mainMenu.addItem(appMenuItem)
+
+        let editMenuItem = NSMenuItem()
+        let editMenu = NSMenu(title: "Edit")
+        editMenu.addItem(withTitle: "Undo", action: Selector(("undo:")), keyEquivalent: "z")
+        editMenu.addItem(withTitle: "Redo", action: Selector(("redo:")), keyEquivalent: "Z")
+        editMenu.addItem(.separator())
+        editMenu.addItem(withTitle: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x")
+        editMenu.addItem(withTitle: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c")
+        editMenu.addItem(withTitle: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
+        editMenu.addItem(withTitle: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
+        editMenuItem.submenu = editMenu
+        mainMenu.addItem(editMenuItem)
+
+        NSApp.mainMenu = mainMenu
     }
 
     @objc func reloadConfig() {
